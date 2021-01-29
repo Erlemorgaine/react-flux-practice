4c00a660586c0db6c8d5b790ef76445eaf1d0339
Erlemorgaine <erle_3@hotmail.com>
2021-01-29 11:01:39 +0100

    add validation, enable editing course

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Prompt } from "react-router-dom";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
@@ -13,23 +13,54 @@ const ManageCoursePage = (props) => {
         category: ""
     });
 
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        const slug = props.match.params.slug;
+
+        if (slug) {
+            courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+        }
+        
+    }, [props.match.params.slug]);
+
     function handleCourseChange({target}) {
         setCourse({...course, [target.name]: target.value});
     }
 
+    function formIsValid() {
+        const _errors = {};
+
+        if (!course.title) _errors.title = "Title is required";
+        if (!course.authorId) _errors.authorId = "Author is required";
+        if (!course.category) _errors.category = "Category is required";
+
+        setErrors(_errors);
+        // Return true if there are no errors
+        return Object.keys(_errors).length === 0;
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        courseApi.saveCourse(course).then(() => {
-            props.history.push("/courses");
-            toast.success("Course saved");
-        });
+
+        if (formIsValid()) {
+            courseApi.saveCourse(course).then(() => {
+                props.history.push("/courses");
+                toast.success("Course saved");
+            });
+        };
     }
 
     return (
         <>
             <h2>Manage course</h2>
             <Prompt when={true} message="Are you sure you want to leave?"/>
-            <CourseForm course={course} onCourseChange={handleCourseChange} onSubmit={handleSubmit}/>
+            <CourseForm 
+                course={course} 
+                onCourseChange={handleCourseChange} 
+                onSubmit={handleSubmit}
+                errors={errors}
+            />
         </>
     );
 }
